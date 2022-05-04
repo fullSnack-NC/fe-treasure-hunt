61743301ce4c4c9bbe57f88c4cbbbc91131d83e3
Gareth Everson <gareth@garetheverson.com>
2022-05-04 12:41:44 +0100

    Pushing non-working version of maps in CurrentWaypoing.jsx for debugging

--- treasureHunt/Components/CurrentWaypoint.jsx
+++ treasureHunt/Components/CurrentWaypoint.jsx
@@ -1,15 +1,58 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { PROVIDER_GOOGLE, MapView } from 'react-native-maps';
+import * as Location from 'expo-location';
+import { useState, useEffect } from 'react';
+import { REACT_APP_MAPS_API_KEY } from '.env';
 
 const CurrentWaypoint = ({ navigation }) => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const apiKey = REACT_APP_MAPS_API_KEY;
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View>
-      <Button title="Found" onPress={() => navigation.push("Certificate")} />
+      <MapView
+        style={{ height: '70%', width: '90%' }}
+        provider={PROVIDER_GOOGLE}
+        apiKey={apiKey}
+        showsUserLocation={true}
+      />
+      <Button title='Found' onPress={() => navigation.push('Certificate')} />
       <Button
         title="Can't find (skip)"
-        onPress={() => navigation.push("Certificate")}
+        onPress={() => navigation.push('Certificate')}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default CurrentWaypoint;
