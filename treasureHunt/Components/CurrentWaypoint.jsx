61572a11ca7c177a62715b2422862ac53205c21b
Jay <jiyoung26091212@gmail.com>
2022-05-04 16:51:01 +0100

    feat : Add google map and static waypoint image to CurrenWaypoint

--- treasureHunt/Components/CurrentWaypoint.jsx
+++ treasureHunt/Components/CurrentWaypoint.jsx
@@ -1,47 +1,75 @@
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, Image, StyleSheet, ScrollView, AppRegistry, Dimensions } from 'react-native';
 import { PROVIDER_GOOGLE } from 'react-native-maps';
 import MapView from 'react-native-maps';
 import * as Location from 'expo-location';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Component } from 'react';
 import { REACT_APP_MAPS_API_KEY } from '@env';
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
+
 const CurrentWaypoint = ({ navigation }) => {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-  const apiKey = REACT_APP_MAPS_API_KEY;
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
-  return (
-    <View>
-      <MapView
-        style={{ height: '90%', width: '90%' }}
-        provider={PROVIDER_GOOGLE}
-        apiKey={apiKey}
-        showsUserLocation={true}
-      />
-    </View>
-  );
+	const [location, setLocation] = useState(null);
+	const [errorMsg, setErrorMsg] = useState(null);
+	const apiKey = REACT_APP_MAPS_API_KEY;
+	const screenWidth = Dimensions.get('window').width;
+	const screenHeight = Dimensions.get('window').height;
+
+	useEffect(() => {
+		(async () => {
+			let { status } = await Location.requestForegroundPermissionsAsync();
+			if (status !== 'granted') {
+				setErrorMsg('Permission to access location was denied');
+				return;
+			}
+			let location = await Location.getCurrentPositionAsync({});
+			setLocation(location);
+		})();
+	}, []);
+
+	let text = 'Waiting..';
+
+	if (errorMsg) {
+		text = errorMsg;
+	} else if (location) {
+		text = JSON.stringify(location);
+	}
+
+	return (
+		<ScrollView horizontal={true} pagingEnabled={true}>
+			<View>
+				<Image
+					source={require('../assets/waypoint-images/Waypoint_1_Roundhay_Park.png')}
+					resizeMode='contain'
+					style={{
+						flex: 1,
+						height: '90%',
+						width: screenWidth,
+						justifyContent: 'center',
+						alignItems: 'center',
+						// backgroundColor: 'tomato',
+					}}
+				/>
+			</View>
+			<View
+				style={{
+					flex: 1,
+					height: screenHeight,
+					width: screenWidth,
+					justifyContent: 'center',
+					alignItems: 'center',
+					backgroundColor: 'blue',
+				}}
+			>
+				<MapView style={{ height: screenHeight - 40, width: screenWidth - 40 }} provider={PROVIDER_GOOGLE} apiKey={apiKey} showsUserLocation={true} />
+			</View>
+		</ScrollView>
+	);
 };
 export default CurrentWaypoint;
