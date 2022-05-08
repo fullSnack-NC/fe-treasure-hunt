420d558a18b5ebd8a4b89ffdf0c683fd7345f7ab
iblis <chris.strafford@gmail.com>
2022-05-08 13:59:58 +0100

    vibration working on click, button functions bundled

--- treasureHunt/Components/Welcome.jsx
+++ treasureHunt/Components/Welcome.jsx
@@ -1,31 +1,51 @@
-import { View, Text, Button, Image, StyleSheet } from 'react-native';
+import { View, Text, Button, Image, StyleSheet, Vibration } from "react-native";
+
+const VibrationPattern = () => {
+  const duration = 5000;
+  const pattern = [1000, 2000, 1000, 2000];
+};
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: 10,
     flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
   },
   welcomeImage: {
     flex: 1,
-    width: '80%',
+    width: "80%",
   },
   button: {
-    width: '70%',
-    backgroundColor: 'blue',
+    width: "70%",
+    backgroundColor: "blue",
   },
 });
 
 const Welcome = ({ navigation }) => {
+  const duration = 1000;
+  navigateGo = () => navigation.navigate("GamePage");
+  vibeGo = () => Vibration.vibrate(duration);
+  navigateBundle = () => {
+    this.navigateGo();
+    this.vibeGo();
+  };
+
+  locationGo = () => navigation.navigate("Locations");
+  vibeGo = () => Vibration.vibrate(duration);
+  locationBundle = () => {
+    this.locationGo();
+    this.vibeGo();
+  };
+
   return (
     <View style={styles.container}>
       <Text>Welcome to Treasure Hunt</Text>
       <Image
         style={styles.welcomeImage}
-        source={require('../assets/welcome.png')}
-        resizeMode='contain'
+        source={require("../assets/welcome.png")}
+        resizeMode="contain"
       />
       <Text>
         This is a game for young families to help children understand the world
@@ -33,13 +53,13 @@ const Welcome = ({ navigation }) => {
       </Text>
       <Button
         style={styles.button}
-        title='Locations'
-        onPress={() => navigation.navigate('Locations')}
+        title="Locations"
+        onPress={() => this.locationBundle()}
       />
       <Button
         style={styles.button}
-        title='Go to game page'
-        onPress={() => navigation.navigate('GamePage')}
+        title="Go to game page"
+        onPress={() => this.navigateBundle()}
       />
     </View>
   );
