d7f7bae54265127111311a38ac1cc2cb816c4bb0
Gareth Everson <gareth@garetheverson.com>
2022-05-08 17:31:22 +0100

    CurrentWaypoint: Reducing refresh timer to 2 seconds and adding sample waypoint marker, 'waypointPosition'

--- treasureHunt/Components/CurrentWaypoint.jsx
+++ treasureHunt/Components/CurrentWaypoint.jsx
@@ -15,118 +15,128 @@ import globalStyles from '../css/style';
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
 
 const CurrentWaypoint = () => {
-	const [location, setLocation] = useState({
-		latitude: 0,
-		longitude: 0,
-	});
+  const [location, setLocation] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
 
-	const [region, setRegion] = useState({
-		latitude: 0,
-		longitude: 0,
-		latitudeDelta: 0.003922,
-		longitudeDelta: 0.003421,
-	});
+  const [region, setRegion] = useState({
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 0.003922,
+    longitudeDelta: 0.003421,
+  });
 
-	const [errorMsg, setErrorMsg] = useState(null);
-	const apiKey = REACT_APP_MAPS_API_KEY;
-	const screenWidth = Dimensions.get('window').width;
-	const screenHeight = Dimensions.get('window').height;
+  const [errorMsg, setErrorMsg] = useState(null);
+  const apiKey = REACT_APP_MAPS_API_KEY;
+  const screenWidth = Dimensions.get('window').width;
+  const screenHeight = Dimensions.get('window').height;
 
-	useEffect(() => {
-		async () => {
-			let { status } = await Location.requestForegroundPermissionsAsync();
-			if (status !== 'granted') {
-				setErrorMsg('Permission to access location was denied');
-				return;
-			}
-		};
-		const getLocation = async () => {
-			const location = await Location.getCurrentPositionAsync({});
-			setLocation({
-				latitude: location.coords.latitude,
-				longitude: location.coords.longitude,
-			});
-			setRegion({
-				latitude: location.coords.latitude,
-				longitude: location.coords.longitude,
-				latitudeDelta: 0.003922,
-				longitudeDelta: 0.003421,
-			});
-		};
+  const waypointPosition = {
+    // needs to be dynamically updated to next waypoint
+    latitude: 53.839277,
+    longitude: -1.496882,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  };
 
-		// const timer = setTimeout(() => {
-		// 	getLocation();
-		// }, 5000);
+  useEffect(() => {
+    async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+    };
+    const getLocation = async () => {
+      const location = await Location.getCurrentPositionAsync({});
+      setLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      });
+      setRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.003922,
+        longitudeDelta: 0.003421,
+      });
+    };
 
-		// return () => clearTimeout(timer);
-	}, [location, region]);
+    const timer = setTimeout(() => {
+      getLocation();
+    }, 2000);
 
-	let text = 'Waiting..';
+    return () => clearTimeout(timer);
+  }, [location, region]);
 
-	if (errorMsg) {
-		text = errorMsg;
-	} else if (location) {
-		text = JSON.stringify(location);
-	}
+  let text = 'Waiting..';
 
-	return (
-		<ScrollView horizontal={true} pagingEnabled={true}>
-			<View>
-				<Image
-					source={require('../assets/waypoint-images/Waypoint_1_Roundhay_Park.png')}
-					resizeMode='contain'
-					style={{
-						flex: 1,
-						height: '90%',
-						width: screenWidth,
-						justifyContent: 'center',
-						alignItems: 'center',
-					}}
-				/>
-				<TouchableOpacity style={globalStyles.baseBtn}>
-					<Text style={globalStyles.btnText}>Found </Text>
-				</TouchableOpacity>
-				<TouchableOpacity style={globalStyles.baseBtn}>
-					<Text style={globalStyles.btnText}>Find the next treasure</Text>
-				</TouchableOpacity>
-			</View>
-			<View
-				pointerEvents='none'
-				style={{
-					flex: 1,
-					height: screenHeight,
-					width: screenWidth,
-					justifyContent: 'center',
-					alignItems: 'center',
-					backgroundColor: 'blue',
-				}}
-			>
-				{location && (
-					<MapView
-						style={{
-							height: screenHeight - 40,
-							width: screenWidth - 40,
-						}}
-						provider={PROVIDER_GOOGLE}
-						apiKey={apiKey}
-						region={region}
-						showsUserLocation={true}
-						scrollEnabled={false}
-						rotateEnabled={true}
-						mapType='satellite'
-					/>
-				)}
-			</View>
-		</ScrollView>
-	);
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  return (
+    <ScrollView horizontal={true} pagingEnabled={true}>
+      <View>
+        <Image
+          source={require('../assets/waypoint-images/Waypoint_1_Roundhay_Park.png')}
+          resizeMode='contain'
+          style={{
+            flex: 1,
+            height: '90%',
+            width: screenWidth,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        />
+        <TouchableOpacity style={globalStyles.baseBtn}>
+          <Text style={globalStyles.btnText}>Found </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={globalStyles.baseBtn}>
+          <Text style={globalStyles.btnText}>Find the next treasure</Text>
+        </TouchableOpacity>
+      </View>
+      <View
+        pointerEvents='none'
+        style={{
+          flex: 1,
+          height: screenHeight,
+          width: screenWidth,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'blue',
+        }}
+      >
+        {location && (
+          <MapView
+            style={{
+              height: screenHeight - 40,
+              width: screenWidth - 40,
+            }}
+            provider={PROVIDER_GOOGLE}
+            apiKey={apiKey}
+            region={region}
+            showsUserLocation={true}
+            scrollEnabled={false}
+            rotateEnabled={true}
+            mapType='satellite'
+          >
+            <Marker coordinate={waypointPosition} />
+          </MapView>
+        )}
+      </View>
+    </ScrollView>
+  );
 };
 export default CurrentWaypoint;
