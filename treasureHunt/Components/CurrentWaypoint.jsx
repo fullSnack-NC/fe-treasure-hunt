c566a44fa08d2d62d945e448395f397f03ec9e15
Jay <jiyoung26091212@gmail.com>
2022-05-09 09:58:16 +0100

    working on adding waypoints

--- treasureHunt/Components/CurrentWaypoint.jsx
+++ treasureHunt/Components/CurrentWaypoint.jsx
@@ -24,119 +24,129 @@ const styles = StyleSheet.create({
 });
 
 const CurrentWaypoint = () => {
-  const [location, setLocation] = useState({
-    latitude: 0,
-    longitude: 0,
-  });
+  const waypointPositions = [
+		{ wayPoint_id: 1, latitude: 53.839277, longitude: -1.496882, latitudeDelta: 0.01, longitudeDelta: 0.01 },
+		{ wayPoint_id: 2, latitude: 53.837302, longitude: -1.498502, latitudeDelta: 0.01, longitudeDelta: 0.01 },
+		{ wayPoint_id: 3, latitude: 53.83808, longitude: -1.502912, latitudeDelta: 0.01, longitudeDelta: 0.01 },
+		{ wayPoint_id: 4, latitude: 53.838978, longitude: -1.499773, latitudeDelta: 0.01, longitudeDelta: 0.01 },
+		{ wayPoint_id: 5, latitude: 53.839108, longitude: -1.49662, latitudeDelta: 0.01, longitudeDelta: 0.01 },
+	];
 
-  const [region, setRegion] = useState({
-    latitude: 0,
-    longitude: 0,
-    latitudeDelta: 0.003922,
-    longitudeDelta: 0.003421,
-  });
+	const [currentWaypointPosition, setCurrentWaypointPosition] = useState(waypointPositions[0]);
+	console.log(currentWaypointPosition);
+	const [location, setLocation] = useState({
+		latitude: 0,
+		longitude: 0,
+	});
 
-  const [errorMsg, setErrorMsg] = useState(null);
-  const apiKey = REACT_APP_MAPS_API_KEY;
-  const screenWidth = Dimensions.get('window').width;
-  const screenHeight = Dimensions.get('window').height;
+	const [region, setRegion] = useState({
+		latitude: 0,
+		longitude: 0,
+		latitudeDelta: 0.003922,
+		longitudeDelta: 0.003421,
+	});
 
-  const waypointPosition = {
-    // needs to be dynamically updated to next waypoint
-    latitude: 53.839277,
-    longitude: -1.496882,
-    latitudeDelta: 0.01,
-    longitudeDelta: 0.01,
-  };
+	const [errorMsg, setErrorMsg] = useState(null);
+	const apiKey = REACT_APP_MAPS_API_KEY;
+	const screenWidth = Dimensions.get('window').width;
+	const screenHeight = Dimensions.get('window').height;
 
-  useEffect(() => {
-    async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-    };
-    const getLocation = async () => {
-      const location = await Location.getCurrentPositionAsync({});
-      setLocation({
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-      });
-      setRegion({
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-        latitudeDelta: 0.003922,
-        longitudeDelta: 0.003421,
-      });
-    };
+	useEffect(() => {
+		async () => {};
+	}, []);
 
-    const timer = setTimeout(() => {
-      getLocation();
-    }, 2000);
+	useEffect(() => {
+		async () => {
+			let { status } = await Location.requestForegroundPermissionsAsync();
+			if (status !== 'granted') {
+				setErrorMsg('Permission to access location was denied');
+				return;
+			}
+		};
+		const getLocation = async () => {
+			const location = await Location.getCurrentPositionAsync({});
+			setLocation({
+				latitude: location.coords.latitude,
+				longitude: location.coords.longitude,
+			});
+			setRegion({
+				latitude: location.coords.latitude,
+				longitude: location.coords.longitude,
+				latitudeDelta: 0.003922,
+				longitudeDelta: 0.003421,
+			});
+		};
 
-    return () => clearTimeout(timer);
-  }, [location, region]);
+		const timer = setTimeout(() => {
+			getLocation();
+		}, 2000);
 
-  let text = 'Waiting..';
+		return () => clearTimeout(timer);
+	}, [location, region]);
 
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
+	const handlePress = (wayPoint_id) => {
+		setCurrentWaypointPosition(wayPoint_id + 1);
+	};
 
-  return (
-    <ScrollView horizontal={true} pagingEnabled={true}>
-      <View>
-        <Image
-          source={require('../assets/waypoint-images/Waypoint_1_Roundhay_Park.png')}
-          resizeMode='contain'
-          style={{
-            flex: 1,
-            height: '90%',
-            width: screenWidth,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        />
-        <TouchableOpacity style={globalStyles.baseBtn}>
-          <Text style={globalStyles.btnText}>Found </Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={globalStyles.baseBtn}>
-          <Text style={globalStyles.btnText}>Find the next treasure</Text>
-        </TouchableOpacity>
-      </View>
-      <View
-        pointerEvents='none'
-        style={{
-          flex: 1,
-          height: screenHeight,
-          width: screenWidth,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: 'blue',
-        }}
-      >
-        {location && (
-          <MapView
-            style={{
-              height: screenHeight - 40,
-              width: screenWidth - 40,
-            }}
-            provider={PROVIDER_GOOGLE}
-            apiKey={apiKey}
-            region={region}
-            showsUserLocation={true}
-            scrollEnabled={false}
-            rotateEnabled={true}
-            mapType='satellite'
-          >
-            <Marker coordinate={waypointPosition} />
-          </MapView>
-        )}
-      </View>
-    </ScrollView>
-  );
+	let text = 'Waiting..';
+
+	if (errorMsg) {
+		text = errorMsg;
+	} else if (location) {
+		text = JSON.stringify(location);
+	}
+
+	return (
+		<ScrollView horizontal={true} pagingEnabled={true}>
+			<View>
+				<Image
+					source={require('../assets/waypoint-images/Waypoint_1_Roundhay_Park.png')}
+					resizeMode='contain'
+					style={{
+						flex: 1,
+						height: '90%',
+						width: screenWidth,
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				/>
+				<TouchableOpacity style={globalStyles.baseBtn} onPress={() => handlePress(wayPoint_id)}>
+					<Text style={globalStyles.btnText}>Found</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={globalStyles.baseBtn}>
+					<Text style={globalStyles.btnText}>Find the next treasure</Text>
+				</TouchableOpacity>
+			</View>
+			<View
+				pointerEvents='none'
+				style={{
+					flex: 1,
+					height: screenHeight,
+					width: screenWidth,
+					justifyContent: 'center',
+					alignItems: 'center',
+					backgroundColor: 'blue',
+				}}
+			>
+				{location && (
+					<MapView
+						style={{
+							height: screenHeight - 40,
+							width: screenWidth - 40,
+						}}
+						provider={PROVIDER_GOOGLE}
+						apiKey={apiKey}
+						region={region}
+						showsUserLocation={true}
+						scrollEnabled={false}
+						rotateEnabled={true}
+						mapType='satellite'
+					>
+						<Marker coordinate={currentWaypointPosition} />
+					</MapView>
+				)}
+			</View>
+		</ScrollView>
+	);
 };
 export default CurrentWaypoint;
