b9c2a3de2a0c3c625b168a6ff760a46cc076ada4
iblis <chris.strafford@gmail.com>
2022-05-09 16:16:54 +0100

    hunt card deleted

--- treasureHunt/Components/HuntList.jsx
+++ treasureHunt/Components/HuntList.jsx
@@ -1,55 +1,55 @@
-import { useEffect, useState } from 'react';
-import { View, Text, Button } from 'react-native';
-import HuntCard from './HuntCard';
-import { getMapsByParkID } from '../utils/api';
+import { useEffect, useState } from "react";
+import { View, Text, Button } from "react-native";
+import HuntCard from "./HuntCard";
+import { getMapsByParkID } from "../utils/api";
 
 const HuntList = ({ route, navigation }) => {
-	const [maps, setMaps] = useState([]);
-	const [error, setError] = useState(null);
-	const [isLoading, setIsLoading] = useState(true);
-	const { park_id } = route.params;
+  const [maps, setMaps] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const { park_id } = route.params;
 
-	useEffect(() => {
-		getMapsByParkID(park_id)
-			.then((data) => {
-				setMaps(data);
-				setIsLoading(false);
-				setError(null);
-			})
-			.catch((err) => {
-				console.log(err.response.data);
-				setError({ err });
-				setIsLoading(false);
-			});
-	}, []);
+  useEffect(() => {
+    getMapsByParkID(park_id)
+      .then((data) => {
+        setMaps(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+        setError({ err });
+        setIsLoading(false);
+      });
+  }, []);
 
-	if (isLoading) {
-		return <Text>Maps Loading...</Text>;
-	}
+  if (isLoading) {
+    return <Text>Maps Loading...</Text>;
+  }
 
-	if (error) {
-		return <Text>Parks not found</Text>;
-	}
-	return (
-		<View>
-			{maps.map((map) => {
-				return (
-					<View key={map.map_id}>
-						<Text>{map.map_name}</Text>
-						<Text>{map.length} KM</Text>
-						<Text>{map.est_comp_time} mins</Text>
-						<Text>Suitable for {map.age_min} +</Text>
+  if (error) {
+    return <Text>Parks not found</Text>;
+  }
+  return (
+    <View>
+      {maps.map((map) => {
+        return (
+          <View key={map.map_id}>
+            <Text>{map.map_name}</Text>
+            <Text>{map.length} KM</Text>
+            <Text>{map.est_comp_time} mins</Text>
+            <Text>Suitable for {map.age_min} +</Text>
 
-						<Button
-							title='Select this Hunt'
-							onPress={() =>
-								navigation.push('GameLaunchHandover', { map_id: map.map_id })
-							}
-						/>
-					</View>
-				);
-			})}
-		</View>
-	);
+            <Button
+              title="Select this Hunt"
+              onPress={() =>
+                navigation.push("GameLaunchHandover", { map_id: map.map_id })
+              }
+            />
+          </View>
+        );
+      })}
+    </View>
+  );
 };
 export default HuntList;
