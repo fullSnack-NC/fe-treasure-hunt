42c6aae1018d36d127218035c191e19b0694a5b2
almcw <98993527+almcw@users.noreply.github.com>
2022-05-09 12:54:12 +0100

    not working for am

--- treasureHunt/Components/GameLaunchHandover.jsx
+++ treasureHunt/Components/GameLaunchHandover.jsx
@@ -1,12 +1,16 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button } from 'react-native';
 
 const GameLaunchHandover = ({ navigation }) => {
   return (
     <View>
       <Text>GameLaunchHandover</Text>
+      <Text>
+        OK Ranger - let's GO! Look at the picture. Look at the map. Find the
+        landmarks.
+      </Text>
       <Button
-        title="Start Gameplay"
-        onPress={() => navigation.push("GamePage")}
+        title='Start Gameplay'
+        onPress={() => navigation.push('GamePage')}
       />
     </View>
   );
