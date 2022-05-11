39d06bb5a7a3c9a49e5e7617417afd6aaa672841
nicolalockwood <nicolahelenlockwood@gamil.com>
2022-05-11 17:57:35 +0100

    Hunt cards added and formatted

--- treasureHunt/Components/HuntList.jsx
+++ treasureHunt/Components/HuntList.jsx
@@ -1,5 +1,15 @@
 import { useEffect, useState } from 'react';
-import { View, Text, Button, Image } from 'react-native';
+import {
+	View,
+	Text,
+	Button,
+	Image,
+	StyleSheet,
+	ImageBackground,
+	Dimensions,
+	ScrollView,
+	TouchableOpacity,
+} from 'react-native';
 import { getMapsByParkID } from '../utils/api';
 
 const HuntList = ({ route, navigation }) => {
@@ -58,28 +68,111 @@ const HuntList = ({ route, navigation }) => {
 		return <Text>Parks not found</Text>;
 	}
 	return (
-		<View>
-			{maps.map((map) => {
-				return (
-					<View key={map.map_id}>
-						<Text>{map.map_name}</Text>
-						<Image
-							source={mapImage[map.map_id - 1].image}
-							style={{ height: 50, width: 150 }}
-						/>
-						<Text>{map.length} KM</Text>
-						<Text>{map.est_comp_time} mins</Text>
-						<Text>Suitable for {map.age_min} +</Text>
-						<Button
-							title='Select this Hunt'
-							onPress={() =>
-								navigation.push('GameLaunchHandover', { map_id: map.map_id })
-							}
-						/>
-					</View>
-				);
-			})}
+		<View style={styles.container}>
+			<ImageBackground
+				source={require('../assets/view-images/background-noLamp.png')}
+				resizeMode='contain'
+				style={styles.image}
+				imageStyle={styles.image_imageStyle}
+			>
+				<>
+					<ScrollView style={[styles.cardList]}>
+						{maps.map((map) => {
+							return (
+								<TouchableOpacity
+									key={map.map_id}
+									onPress={() =>
+										navigation.push('GameLaunchHandover', {
+											map_id: map.map_id,
+										})
+									}
+								>
+									<View
+										key={map.map_id}
+										style={[cardStyles.container, styles.materialCard51]}
+									>
+										<Image
+											source={mapImage[map.map_id - 1].image}
+											style={cardStyles.cardItemImagePlace}
+										/>
+										<View style={cardStyles.bodyContent}>
+											<Text style={cardStyles.titleStyle}>{map.map_name}</Text>
+											<Text style={cardStyles.subtitleStyle}>
+												{map.length} KM {map.est_comp_time} mins Suitable for{' '}
+												{map.age_min} +
+											</Text>
+										</View>
+									</View>
+								</TouchableOpacity>
+							);
+						})}
+					</ScrollView>
+				</>
+			</ImageBackground>
 		</View>
 	);
 };
+
+const cardStyles = StyleSheet.create({
+	container: {
+		borderWidth: 1,
+		borderRadius: 2,
+		borderColor: '#CCC',
+		flexWrap: 'nowrap',
+		backgroundColor: '#FFF',
+		shadowColor: '#000',
+		shadowOffset: {
+			width: -2,
+			height: 2,
+		},
+		shadowOpacity: 0.1,
+		shadowRadius: 1.5,
+		elevation: 3,
+		overflow: 'hidden',
+	},
+	cardItemImagePlace: {
+		backgroundColor: '#ccc',
+		flex: 1,
+		minHeight: 210,
+	},
+	bodyContent: {
+		padding: 16,
+		paddingTop: 24,
+		justifyContent: 'center',
+		backgroundColor: 'rgba(124,168,91,1)',
+	},
+	titleStyle: {
+		fontSize: 24,
+		color: 'rgba(255,255,255,1)',
+		paddingBottom: 12,
+	},
+	subtitleStyle: {
+		fontSize: 14,
+		color: 'rgba(255,255,255,1)',
+		lineHeight: 16,
+		opacity: 0.5,
+	},
+});
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	image: {
+		width: 580,
+		height: 847,
+		marginTop: -35,
+		marginLeft: -111,
+	},
+	image_imageStyle: {},
+
+	materialCard51: {
+		height: 350,
+		width: 359,
+		backgroundColor: 'rgba(124,168,91,1)',
+		borderRadius: 20,
+		marginLeft: 119,
+		marginBottom: 10,
+	},
+});
 export default HuntList;
